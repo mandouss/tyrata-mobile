8223154a8abf67717f2e63769d75abfe19cbb0e7
my113 <my113@duke.edu>
2018-03-18 02:03:42 -0400

    Replace Vehicle_Info.java

--- app/src/main/java/edu/duke/ece651/tyrata/display/Vehicle_Info.java
+++ app/src/main/java/edu/duke/ece651/tyrata/display/Vehicle_Info.java
@@ -1,19 +1,36 @@
 package edu.duke.ece651.tyrata.display;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ClipData;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import edu.duke.ece651.tyrata.R;
 import edu.duke.ece651.tyrata.calibration.Input_Vehicle_Info;
 
-public class Vehicle_Info extends AppCompatActivity {
+public class Vehicle_Info extends Activity {
     private Integer buttonnumber = 0;
-    private Button left3;
-    private Button right3;
+
+    private ListView tire_list;
+    private List<Map<String, Object>> list;
+    int axis_row = 0;
+    char axis_side;
+    int axis_index = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,12 +76,123 @@ public class Vehicle_Info extends AppCompatActivity {
         TextView textView_tirenumber = findViewById(R.id.textView_tirenumber);
         textView_tirenumber.setText(message_tirenumber);
 
+        int axis_num = intent.getIntExtra("AXIS_NUM",0);
+
         buttonnumber=Integer.parseInt(message_tirenumber);
-        left3 = (Button)findViewById(R.id.button_letf3);
-        right3 = (Button)findViewById(R.id.button_right3);
-        if( buttonnumber==6){
-            left3.setVisibility(View.VISIBLE);
-            right3.setVisibility(View.VISIBLE);
+
+
+        tire_list = (ListView) findViewById(R.id.tire_list);
+        initDataList(buttonnumber);
+
+        String[] from = { "img", "tire number", "content", "percent" };
+        // 列表项组件Id 数组
+        int[] to = { R.id.item_img, R.id.item_tire, R.id.item_location,
+                R.id.item_percent };
+        /**
+         * SimpleAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to)
+         * context：activity界面类
+         * data 数组内容是map的集合数据
+         * resource 列表项文件
+         * from map key值数组
+         * to 列表项组件id数组      from与to一一对应，适配器绑定数据
+         */
+        final SimpleAdapter adapter = new SimpleAdapter(this, list,
+                R.layout.list_view_layout, from, to);
+
+        tire_list.setAdapter(adapter);
+        /**
+         * 单击
+         */
+        tire_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                Map<String, Object> map = list.get(arg2);
+
+                String str = "";
+                String str2="";
+                str += map.get("tire number");
+                for(int i=0;i<str.length();i++) {
+                    if (str.charAt(i) >= 48 && str.charAt(i) <= 57) {
+                        str2 += str.charAt(i);
+                    }
+                }
+                int tire_num = Integer.valueOf(str2);
+                calculate_location(buttonnumber,tire_num);
+                vehicle_to_tire();
+
+            }
+        });
+        /**
+         * 长按
+         */
+        tire_list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int arg2, long arg3) {
+                list.remove(arg2);
+                adapter.notifyDataSetChanged();// 更新列表数据
+                Toast.makeText(Vehicle_Info.this, "删除成功！", Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+
+    }
+
+    private void calculate_location(int tirenum,int index){
+        int side = -1;   //left-1,right-0
+        if(tirenum == 4){
+            axis_row = (index-1)/2+1;
+            axis_index = 1;
+            side = index%2;
+        }
+        if(tirenum == 10 || tirenum == 18) {
+            axis_row = (index + 1) / 4 + 1;
+            if (index == 1) {
+                axis_index = 1;
+                side = 1;
+            } else if (index == 2) {
+                axis_index = 1;
+                side = 0;
+            } else {
+                if (index % 4 == 0 || index % 4 == 1) {
+                    axis_index = 1;
+                } else {
+                    axis_index = 2;
+                }
+                if (index % 4 == 0 || index % 4 == 3) {
+                    side = 1;
+                } else {
+                    side = 0;
+                }
+            }
+        }
+        if(side == 1){
+            axis_side = 'l';
+        }
+        else {
+            axis_side = 'r';
+        }
+    }
+
+
+    private void initDataList(int number) {
+        //图片资源
+        int img[] = null;
+        img = new int[number];
+        for(int i = 0;i < number; i++) {
+            img[i] = R.drawable.tire;
+        }
+        list = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < number; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("img", img[i]);
+            map.put("tire number", "tire" + (i+1));
+            map.put("content", "location:" );
+            map.put("percent", "96%");
+            list.add(map);
         }
     }
 
@@ -74,11 +202,25 @@ public class Vehicle_Info extends AppCompatActivity {
         startActivity(intent);
         // Do something in response to button
     }
-    public void vehicle_to_tire (View view) {
+    public void vehicle_to_tire () {
         Intent intent = new Intent(Vehicle_Info.this, TireInfo.class);
+        intent.putExtra("AXIS_ROW", axis_row);
+        intent.putExtra("AXIS_INDEX",axis_index);
+        intent.putExtra("AXIS_SIDE", axis_side);
 
         startActivity(intent);
         // Do something in response to button
+    }
+    public void delete_vehicle(View view){
+        showExitDialog02();
+    }
+    private void showExitDialog02(){
+        new AlertDialog.Builder(this)
+                .setTitle("NOTIFICATION")
+                .setMessage("Are you sure to delete this car from your account?")
+                .setPositiveButton("Yes", null)
+                .setNegativeButton("No", null)
+                .show();
     }
 
 
