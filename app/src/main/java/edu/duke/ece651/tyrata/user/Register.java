2e6ea45fdecfa5c82049126f1692fb0a7ab1cb8a
兰德 <lande@landedeMacBook-Pro.local>
2018-03-18 11:44:32 -0400

    Add info on register page

--- app/src/main/java/edu/duke/ece651/tyrata/user/Register.java
+++ app/src/main/java/edu/duke/ece651/tyrata/user/Register.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import edu.duke.ece651.tyrata.MainActivity;
 import edu.duke.ece651.tyrata.R;
@@ -17,6 +18,16 @@ public class Register extends AppCompatActivity {
     }
     public void Register_to_main(View view) {
         Intent intent = new Intent(Register.this, edu.duke.ece651.tyrata.MainActivity.class);
+
+        EditText username = (EditText) findViewById(R.id.register_edit_username);
+        String message_username = username.getText().toString();
+
+        EditText email = (EditText) findViewById(R.id.register_edit_email);
+        String message_email = email.getText().toString();
+
+        EditText phone = (EditText) findViewById(R.id.register_edit_phonenumber);
+        String message_phone = phone.getText().toString();
+
 
         startActivity(intent);
         // Do something in response to button
